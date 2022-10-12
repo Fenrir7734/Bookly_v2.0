b25d7204bcde7aefc3999a24c30d5adf5419d1d6
Fenrir7734 <hitex1999@gmail.com>
2022-10-12 17:26:51 +0200

    ReviewController tests - not finished

--- backend/src/test/java/com/fenrir/masterdetail/controller/ReviewControllerTest.java
+++ backend/src/test/java/com/fenrir/masterdetail/controller/ReviewControllerTest.java
@@ -1,7 +1,166 @@
 package com.fenrir.masterdetail.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fenrir.masterdetail.dto.ReviewRequestDTO;
+import com.fenrir.masterdetail.repository.ReviewRepository;
+import com.fenrir.masterdetail.setup.IntegrationTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.jdbc.Sql;
 
-class ReviewControllerTest {
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Sql(value = {
+        "classpath:db/ReviewControllerTest.sql"
+})
+class ReviewControllerTest extends IntegrationTest {
+    private static final String REVIEW_CONTROLLER_ENDPOINT = "/api/reviews";
+    private static final String GET_REVIEW_BY_USERNAME_AND_BOOK_ID = REVIEW_CONTROLLER_ENDPOINT + "/{username}/{bookId}";
+    private static final String GET_REVIEW_BY_BOOK_ID = REVIEW_CONTROLLER_ENDPOINT + "/book/{bookId}";
+    private static final String GET_REVIEW_BY_USERNAME = REVIEW_CONTROLLER_ENDPOINT + "/user/{username}";
+    private static final String GET_BOOK_STATISTICS = REVIEW_CONTROLLER_ENDPOINT + "/book/{bookId}/stats";
+    private static final String POST_REVIEW = REVIEW_CONTROLLER_ENDPOINT + "/{username}/{bookId}";
+    private static final String UPDATE_REVIEW = REVIEW_CONTROLLER_ENDPOINT + "/{username}/{bookId}";
+    private static final String DELETE_REVIEW = REVIEW_CONTROLLER_ENDPOINT + "/{username}/{bookId}";
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    private static final int BOOK_ID = 101;
+    private static final int USER_ID = 101;
+    private static final String USER_USERNAME = "nowak";
+
+    @Test
+    public void getReviewByUsernameAndBookId_should_return_review() throws Exception {
+        mockMvc.perform(get(GET_REVIEW_BY_USERNAME_AND_BOOK_ID, USER_USERNAME, BOOK_ID))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").isNotEmpty())
+                .andExpect(jsonPath("$.id.userId", is(USER_ID)))
+                .andExpect(jsonPath("$.id.bookId", is(BOOK_ID)))
+                .andExpect(jsonPath("$.content").isNotEmpty())
+                .andExpect(jsonPath("$.rate").isNotEmpty())
+                .andExpect(jsonPath("$.updatedAt").isNotEmpty())
+                .andExpect(jsonPath("$.createdAt").isNotEmpty())
+                .andExpect(jsonPath("$.book").isNotEmpty())
+                .andExpect(jsonPath("$.user").isNotEmpty());
+    }
+
+    @Test
+    public void getReviewByUsernameAndBookId_should_fail_when_given_wrong_username() throws Exception {
+        mockMvc.perform(get(GET_REVIEW_BY_USERNAME_AND_BOOK_ID, "User", BOOK_ID))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.statusCode", is(404)))
+                .andExpect(jsonPath("$.timestamp").isNotEmpty())
+                .andExpect(jsonPath("$.message").isNotEmpty())
+                .andExpect(jsonPath("$.message").isNotEmpty());
+    }
+
+    @Test
+    public void getReviewByUsernameAndBookId_should_fail_when_given_wrong_bookId() throws Exception {
+        mockMvc.perform(get(GET_REVIEW_BY_USERNAME_AND_BOOK_ID, USER_USERNAME, 1))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.statusCode", is(404)))
+                .andExpect(jsonPath("$.timestamp").isNotEmpty())
+                .andExpect(jsonPath("$.message").isNotEmpty())
+                .andExpect(jsonPath("$.message").isNotEmpty());
+    }
+
+    @Test
+    public void getReviewByBookId_should_return_all_book_reviews() throws Exception {
+        mockMvc.perform(get(GET_REVIEW_BY_BOOK_ID, BOOK_ID))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.totalElements", is(1)))
+                .andExpect(jsonPath("$.content").isNotEmpty())
+                .andExpect(jsonPath("$.content[0].id").isNotEmpty())
+                .andExpect(jsonPath("$.content[0].id.userId", is(USER_ID)))
+                .andExpect(jsonPath("$.content[0].id.bookId", is(BOOK_ID)))
+                .andExpect(jsonPath("$.content[0].content").isNotEmpty())
+                .andExpect(jsonPath("$.content[0].rate").isNotEmpty())
+                .andExpect(jsonPath("$.content[0].updatedAt").isNotEmpty())
+                .andExpect(jsonPath("$.content[0].createdAt").isNotEmpty())
+                .andExpect(jsonPath("$.content[0].book").isNotEmpty())
+                .andExpect(jsonPath("$.content[0].user").isNotEmpty());
+    }
+
+    @Test
+    public void getReviewByBookId_should_fail_when_given_wrong_bookId() throws Exception {
+        mockMvc.perform(get(GET_REVIEW_BY_BOOK_ID, BOOK_ID))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.statusCode", is(404)))
+                .andExpect(jsonPath("$.timestamp").isNotEmpty())
+                .andExpect(jsonPath("$.message").isNotEmpty())
+                .andExpect(jsonPath("$.message").isNotEmpty());
+    }
+
+    @Test
+    public void getReviewByUsername_should_return_all_user_reviews() throws Exception {
+        mockMvc.perform(get(GET_REVIEW_BY_USERNAME, USER_USERNAME))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.totalElements", is(1)))
+                .andExpect(jsonPath("$.content").isNotEmpty())
+                .andExpect(jsonPath("$.content[0].id").isNotEmpty())
+                .andExpect(jsonPath("$.content[0].id.userId", is(USER_ID)))
+                .andExpect(jsonPath("$.content[0].id.bookId", is(BOOK_ID)))
+                .andExpect(jsonPath("$.content[0].content").isNotEmpty())
+                .andExpect(jsonPath("$.content[0].rate").isNotEmpty())
+                .andExpect(jsonPath("$.content[0].updatedAt").isNotEmpty())
+                .andExpect(jsonPath("$.content[0].createdAt").isNotEmpty())
+                .andExpect(jsonPath("$.content[0].book").isNotEmpty())
+                .andExpect(jsonPath("$.content[0].user").isNotEmpty());
+    }
+
+    @Test
+    public void getReviewByUsername_should_fail_when_given_wrong_username() throws Exception {
+        mockMvc.perform(get(GET_REVIEW_BY_USERNAME, "User"))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.statusCode", is(404)))
+                .andExpect(jsonPath("$.timestamp").isNotEmpty())
+                .andExpect(jsonPath("$.message").isNotEmpty())
+                .andExpect(jsonPath("$.message").isNotEmpty());
+    }
+
+    @Test
+    public void getBookStatistics_should_return_book_statistics() throws Exception {
+        mockMvc.perform(get(GET_BOOK_STATISTICS, BOOK_ID))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(BOOK_ID)))
+                .andExpect(jsonPath("$.numberOfRates").isNotEmpty())
+                .andExpect(jsonPath("$.numberOfComments").isNotEmpty())
+                .andExpect(jsonPath("$.rate").isNotEmpty());
+    }
+
+    @Test
+    public void getBookStatistics_should_fail_when_given_wrong_book_id() throws Exception {
+        mockMvc.perform(get(GET_BOOK_STATISTICS, 1))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.statusCode", is(404)))
+                .andExpect(jsonPath("$.timestamp").isNotEmpty())
+                .andExpect(jsonPath("$.message").isNotEmpty())
+                .andExpect(jsonPath("$.message").isNotEmpty());
+    }
+
+    @Test
+    @WithMockUser(value = "user", roles = "USER")
+    public void postShelf_should_create_new_review() throws Exception {
+        ReviewRequestDTO requestDTO = new ReviewRequestDTO("Content", 5);
+
+        mockMvc.perform(post(POST_REVIEW, USER_USERNAME, BOOK_ID)
+                .contentType(MediaType.APPLICATION_JSON)
+                .contentType(objectMapper.writeValueAsString(requestDTO)))
+                .andExpect(status().isCreated())
+                .andDo(print());
+    }
 }
---
+++ backend/src/test/resources/db/ReviewControllerTest.sql
@@ -0,0 +1,24 @@
+INSERT INTO user (id, created_at, email, firstname, lastname, password, role, username)
+VALUES (101, '2022-05-19 09:49:09.832115', 'adam.nowak@gmail.com', 'Adam', 'Nowak',
+        '$2a$10$mQqFLlWERo/a51UoImP9ZuynHVas7WL.cDs0n18oUmIbfv27M4gJK', 'ROLE_USER', 'nowak');
+
+INSERT INTO book (id, author, cover, created_at, description, title, updated_at)
+VALUES (101, 'George R.R. Martin',
+        'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1562726234i/13496.jpg',
+        '2022-05-26 15:32:09.908486',
+        'description',
+        'A Game of Thrones', '2022-05-26 15:32:09.909206');
+
+INSERT INTO book (id, author, cover, created_at, description, title, updated_at)
+VALUES (102, 'James S.A. Corey',
+        'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1407572377i/12591698.jpg',
+        '2022-05-26 15:32:09.959447',
+        'description',
+        'Calibans War', '2022-05-26 15:32:09.959472');
+
+INSERT INTO review (book_id, user_id, content, created_at, rate, updated_at)
+VALUES (101, 101,
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        '2022-05-26 15:32:10.021749', 4, '2022-05-26 15:32:10.021772');
+
+
